aa95c0c577e092a0b5392ad4432f542b16b19d8a
yamada-t-e-ize <t.yamada@e-ize.jp>
2024-10-04 14:25:17 +0900

    flex-shrink実装例更新

--- pages/flex-shrink2/index.vue
+++ pages/flex-shrink2/index.vue
@@ -2,19 +2,17 @@
 
   <p>flex-shrinkの既定値は 1</p>
   <p>全体の幅は 500px で、フレックスアイテムの flex-basis は 120px です。</p>
-  <p>A、B、C には flex-shrink:1 が、D と E には flex-shrink:2 が設定されています。</p>
+  <p>A、B、C には flex-shrink:1 が、D には flex-shrink:2 が、と E にはflex-shrink:0が設定されています。</p>
   <p>D と E の幅は他より狭くなります。</p>
-  <p>結局、120*5-500=100px足りない。なので、100/(1+1+1+2+2)=約14.28pxになり</p>
-  <p>flex-shrink:1のdiv幅は120-14.28=105.72px</p>
-  <p>flex-shrink:2のdiv幅は120-(14.28*2)=91.44だが、</p>
-  <p>全部足すと500.04なってしまうので、0.04をflex-shrink:2のdivから引くので</p>
-  <p>flex-shrink:2のdiv幅は120-(14.28*2)-0.02=91.42</p>
+  <p>結局、120*5-500=100px足りない。なので、100/(1+1+1+2+0)=20pxになり</p>
+  <p>flex-shrink:1のdiv幅は120-20=100px</p>
+  <p>flex-shrink:2のdiv幅は120-(20*2)=80</p>
   <div id="content">
     <div class="box" style="background-color:red;">A</div>
     <div class="box" style="background-color:lightblue;">B</div>
     <div class="box" style="background-color:yellow;">C</div>
     <div class="box1" style="background-color:brown;">D</div>
-    <div class="box1" style="background-color:lightgreen;">E</div>
+    <div class="box2" style="background-color:lightgreen;">E</div>
   </div>
 
 </template>
@@ -35,5 +33,9 @@
 
 .box1 {
   flex-shrink: 2;
+}
+
+.box2 {
+  flex-shrink: 0;
 }
 </style>
